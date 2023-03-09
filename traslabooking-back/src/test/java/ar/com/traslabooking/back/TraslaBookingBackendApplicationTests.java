17a91cd29a261714fe63f36781bda157c1323f1a
Emiliano <emiliano.guido@gmail.com>
2023-03-09 14:53:45 -0300

    start project

---
+++ traslabooking-back/src/test/java/ar/com/traslabooking/back/TraslaBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.traslabooking.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraslaBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
